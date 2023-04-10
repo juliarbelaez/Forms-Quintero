1fe82e160a36567d5504c9c10e275910f3cfaf20
Juliana Quintero <juli.carmesi@gmail.com>
2023-04-09 20:57:23 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EstudiantesModule } from './estudiantes/estudiantes.module';
-import { FormErrorHelperComponent } from './shared/components/form-error-helper/form-error-helper.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    EstudiantesModule,
-  ],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, EstudiantesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/estudiantes/estudiantes.module.ts
+++ src/app/estudiantes/estudiantes.module.ts
@@ -2,21 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstudiantesComponent } from './estudiantes.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormErrorHelperModule } from '../shared/components/form-error-helper/form-error-helper.module';
-
-
 
 @NgModule({
-  declarations: [
-    EstudiantesComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormErrorHelperModule
-  ],
-  exports: [
-    EstudiantesComponent
-  ]
+  declarations: [EstudiantesComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [EstudiantesComponent],
 })
-export class EstudiantesModule { }
+export class EstudiantesModule {}
